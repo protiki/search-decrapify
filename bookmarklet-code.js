a76d77f8aa39de6ee8eeb47c24a13e94dd255b68
protiki <77539344+protiki@users.noreply.github.com>
2024-05-19 16:52:00 +0200

    Delete bookmarklet-code.js

--- bookmarklet-code.js
+++
@@ -1,7 +0,0 @@
-(function() {
-    var url = new URL(window.location.href);
-    if (url.hostname === 'www.google.com' && url.searchParams.has('q')) {
-        url.searchParams.set('udm', '14');
-        window.location.href = url.href;
-    }
-})();
